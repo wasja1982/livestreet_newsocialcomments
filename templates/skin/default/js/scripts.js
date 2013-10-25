30ec8fb29191c7560b2166553214787b798a02ce
wasja1982 <wasja@yandex.ru>
2013-10-25 23:43:21 +0300

    Подчистил JS-код: 1) при работе с Facebook события заменены на вызов функции FB.getLoginStatus (иногда наблюдалась ситуация с пропуском обработки событий); 2) однотипный код вынесен в функции; 3) удалены неиспользуемые права доступа.

--- templates/skin/default/js/scripts.js
+++ templates/skin/default/js/scripts.js
@@ -1,40 +1,19 @@
 function vk_auth(response) {
     if (use_vk_api && response.status === 'connected') {
-        var uid = response.session.mid
-        var sid = response.session.sid
-
-        var get_avatar=function(response){
-            var avatar = response.response[0].photo;
-            $("#form_comment").append("<input type='hidden' name='social_avatar' id='social_avatar' value='"+avatar+"' />");
-            var uid = response.response[0].uid;
-            $("#form_comment").append("<input type='hidden' name='social_profile' id='social_profile' value='https://vk.com/id"+uid+"' />");
-        }
+        var uid = response.session.mid;
+        var name = response.session.user.first_name + ' ' + response.session.user.last_name
 
         VK.Api.call('getProfiles',{
-            uids: uid ,
+            uids: uid,
             fields:'photo'
-        },
-        get_avatar
-        );
-
-        var name = response.session.user.first_name + ' ' + response.session.user.last_name
-        $("#guest_name").attr("value",name);
-        $("#social_info span.name").text(name);
-        $("#social_info span.icon").addClass("small_vk_icon");
-        $("#social_info, #guest_text").show();
-        $("#social_chooser, #capcha, #guest_input, #guest_email").hide();
-        $("#form_comment").append("<input type='hidden' name='social' id='social' value='vk' />");
-        $("#sc_exit").addClass("vk");
-
+        }, function(response){
+            var avatar = response.response[0].photo;
+            var profile = 'https://vk.com/id' + response.response[0].uid;
+            fill_form("vk", name, avatar, profile);
+        });
     } else {
         if (use_fb_api) {
-            FB.init({
-                appId: fb_id,
-                xfbml: true,
-                cookie: true,
-                oauth: true
-            });
-            FB.Event.subscribe('auth.statusChange', facebook_auth);
+            FB.getLoginStatus(facebook_auth, true);
         }
     }
 }
@@ -42,68 +21,42 @@ function vk_auth(response) {
 function facebook_auth(response) {
     if (use_fb_api && response.status === 'connected') {
         var uid = response.authResponse.userID;
-        var token = response.authResponse.accessToken;
 
         FB.api('/me', function(response) {
-            $("#guest_name").attr("value",response.name);
-            $("#social_info span.name").text(response.name);
-            $("#social_info span.icon").addClass("small_fb_icon");
-            $("#social_info, #guest_text").show();
-            $("#social_chooser, #capcha, #guest_input, #guest_email").hide();
-            $("#form_comment").append("<input type='hidden' name='social' id='social' value='fb' />");
-            $("#sc_exit").addClass("fb");
-
             var avatar = 'http://graph.facebook.com/' + response.id + '/picture';
-            $("#form_comment").append("<input type='hidden' name='social_avatar' id='social_avatar' value='"+avatar+"' />");
-            $("#form_comment").append("<input type='hidden' name='social_profile' id='social_profile' value='https://www.facebook.com/profile.php?id="+uid+"' />");
+            var profile = 'https://www.facebook.com/profile.php?id=' + uid;
+            fill_form("fb", response.name, avatar, profile);
         });
-    } else {
-        $("#guest_name").attr("value","");
-        $("#social_info span.name").text("");
-        $("#social_info span.icon").removeClass("small_fb_icon");
-        $("#social_info").hide();
-        $("#social_chooser").show();
-        if (guest_enabled) {
-            $("#capcha, #guest_input, #guest_email, #guest_text").show();
-        } else {
-            $("#capcha, #guest_input, #guest_email, #guest_text").hide();
-        }
-        $("#form_comment").find("#social").remove();
-        $("#sc_exit").removeClass("fb");
-        $("#form_comment").find("#social_avatar").remove();
-        $("#form_comment").find("#social_profile").remove();
-
-        if (use_vk_api) {
-            VK.Auth.getLoginStatus(vk_auth, true);
-        } else if (use_fb_api) {
-            FB.Event.subscribe('auth.statusChange', facebook_auth);
-        }
     }
 }
 
-function check_vk_status(response) {
-    if (response.status != 'connected') {
-        $("#guest_name").attr("value","");
-        $("#social_info span.name").text("");
-        $("#social_info span.icon").removeClass("small_vk_icon");
-        $("#social_info").hide();
-        $("#social_chooser").show();
-        if (guest_enabled) {
-            $("#capcha, #guest_input, #guest_email, #guest_text").show();
-        } else {
-            $("#capcha, #guest_input, #guest_email, #guest_text").hide();
-        }
-        $("#form_comment").find("#social").remove();
-        $("#sc_exit").removeClass("vk");
-        $("#form_comment").find("#social_avatar").remove();
-        $("#form_comment").find("#social_profile").remove();
+function fill_form(type, name, avatar, profile) {
+    $("#guest_name").attr("value",name);
+    $("#social_info span.name").text(name);
+    $("#social_info span.icon").addClass("small_" + type + "_icon");
+    $("#social_info, #guest_text").show();
+    $("#social_chooser, #capcha, #guest_input, #guest_email").hide();
+    $("#form_comment").append("<input type='hidden' name='social' id='social' value='" + type + "' />");
+    $("#sc_exit").addClass(type);
+    $("#form_comment").append("<input type='hidden' name='social_avatar' id='social_avatar' value='"+avatar+"' />");
+    $("#form_comment").append("<input type='hidden' name='social_profile' id='social_profile' value='"+profile+"' />");
+}
 
-        if (use_vk_api) {
-            VK.Auth.getLoginStatus(vk_auth, true);
-        } else if (use_fb_api) {
-            FB.Event.subscribe('auth.statusChange', facebook_auth);
-        }
+function clear_form(type) {
+    $("#guest_name").attr("value","");
+    $("#social_info span.name").text("");
+    $("#social_info span.icon").removeClass("small_" + type + "_icon");
+    $("#social_info").hide();
+    $("#social_chooser").show();
+    if (guest_enabled) {
+        $("#capcha, #guest_input, #guest_email, #guest_text").show();
+    } else {
+        $("#capcha, #guest_input, #guest_email, #guest_text").hide();
     }
+    $("#form_comment").find("#social").remove();
+    $("#sc_exit").removeClass(type);
+    $("#form_comment").find("#social_avatar").remove();
+    $("#form_comment").find("#social_profile").remove();
 }
 
 $(function() {
@@ -114,36 +67,52 @@ $(function() {
     } else {
         $("#capcha, #guest_input, #guest_email, #guest_text").hide();
     }
-    $('.login.small_vk_icon').live("click",function(){
-        VK.Auth.login(vk_auth, 1027);
-        return false
-    });
-    $('.login.small_fb_icon').live("click",function(){
-        FB.login(function(){}, {
-            scope : 'user_relationships,publish_stream,offline_access'
-        })
-        return false
-    });
-    $('#sc_exit.vk').live("click",function(){
-        VK.Auth.logout(check_vk_status);
-        return false;
-    });
-    $('#sc_exit.fb').live("click",function(){
-        FB.logout(facebook_auth);
-        return false;
-    });
     if (use_vk_api) {
         VK.init({
             apiId: vk_id
         });
-        VK.Auth.getLoginStatus(vk_auth, true);
-    } else if (use_fb_api) {
+    }
+    if (use_fb_api) {
         FB.init({
             appId: fb_id,
             xfbml: true,
             cookie: true,
             oauth: true
         });
-        FB.Event.subscribe('auth.statusChange', facebook_auth);
+    }
+    $('.login.small_vk_icon').live("click",function(){
+        VK.Auth.login(vk_auth, 0);
+        return false
+    });
+    $('.login.small_fb_icon').live("click",function(){
+        FB.login(function(){
+            FB.getLoginStatus(facebook_auth, true);
+        }, {
+            scope : ''
+        })
+        return false
+    });
+    $('#sc_exit.vk').live("click",function(){
+        clear_form("vk");
+        VK.Auth.logout(function(){
+            if (use_fb_api) {
+                FB.getLoginStatus(facebook_auth, true);
+            }
+        });
+        return false;
+    });
+    $('#sc_exit.fb').live("click",function(){
+        clear_form("fb");
+        FB.logout(function(){
+            if (use_vk_api) {
+                VK.Auth.getLoginStatus(vk_auth, true);
+            }
+        });
+        return false;
+    });
+    if (use_vk_api) {
+        VK.Auth.getLoginStatus(vk_auth, true);
+    } else if (use_fb_api) {
+        FB.getLoginStatus(facebook_auth, true);
     }
 });
